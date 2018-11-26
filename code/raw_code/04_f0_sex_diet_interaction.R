e39ad76ed07e6aa656768a93ec0096db02ad1c67
ManuTel <manumaddy321@gmail.com>
2018-11-26 11:09:46 +0530

    -- sex*diet interaction model for F0 -- got more genes with same signi cut-off

--- code/raw_code/04_f0_sex_diet_interaction.R
+++ code/raw_code/04_f0_sex_diet_interaction.R
@@ -44,4 +44,47 @@ cont.matrix
 fit2 <- contrasts.fit(fit, cont.matrix)
 fit2 <- eBayes(fit2)
 print(topTable(fit2, coef = 3, sort.by = "p", genelist = link, number = 10))
-## all three coef from fit2 model are of important for us to study since they represent HSvsLs in female, male and interaction of diet with sex
+## all three coef from fit2 model are of important for us to study 
+## since they represent HSvsLs in female, male and interaction of diet with sex
+
+## Effect of HS diet in Female
+f0_hs_female   <- topTable(fit2, coef = 1, sort.by = "none", genelist = link, number = Inf)
+f0_hs_female$group <- "female"
+
+## Effect of HS diet in Male
+f0_hs_male     <- topTable(fit2, coef = 2, sort.by = "none", genelist = link, number = Inf)
+f0_hs_male$group <- "male"
+
+## Interaction of HS diet and male sex
+f0_hs_male_int <- topTable(fit2, coef = 3, sort.by = "none", genelist = link, number = Inf) 
+f0_hs_male_int$group <- "HSmale"
+all_df   <- lapply(ls(pattern = "^f0_hs*"), get)
+library(tibble)
+all_df   <- map2(all_df, "probe_id", rownames_to_column)
+all_fc   <- bind_rows(all_df)
+
+#write.csv(all_fc, file = "./data/tidy_data/f0_diet_sex_interaction_fc.csv", row.names = FALSE)
+
+## Exploratory data analysis ####
+summary(all_fc)
+updown <- all_fc %>% 
+            group_by(group) %>%
+              filter(P.Value <  0.05 &  ## NOTE: adjusted p value used
+                      2^abs(logFC) >= 1.3) %>% ## NOTE: fold change 1.3
+              summarise(n = n(), up = sum(logFC > 0),
+                               down = sum(logFC < 0))
+## stringent criteria of 2-fold change and adjusted p-value cut-off 0.05
+
+sign_fc <- all_fc %>%
+            group_by(group) %>%
+            filter(adj.P.Val < 0.05 &
+                     2^abs(logFC) > 2)
+        
+
+Reduce(intersect, split(abc$ID, abc$group))
+## compare with normal HS vs Ls comparison per sex
+fc   <- read.csv("./data/tidy_data/foldchange_df.csv")
+f0_in <- read.csv("./data/tidy_data/f0_diet_sex_interaction_fc.csv")
+# the fold change is similar across both the model (interaction and normal HS vs LS)
+# but interaction model has more significant results as compare to normal model
+## do check for F1 and F2 generation
