7cbcfdc0940d037d56b536aae742c72b11c04eff
ManuTel <manumaddy321@gmail.com>
2018-11-30 13:19:56 +0530

    -- tibble package added for rownames_to_column -- batch information added in phenodata.txt

--- code/raw_code/00_packages.R
+++ code/raw_code/00_packages.R
@@ -1,7 +1,7 @@
 ##install necessary packages if not installed
 
 list_packages <- c("ggplot2", "dplyr", "purrr", "ggpubr", "ggsci", "gridExtra",
-                       "broom", "tidyr")
+                       "broom", "tidyr", "tibble")
 new_packages <- list_packages[!(list_packages %in% installed.packages()[,"Package"])]
 if(length(new_packages)) install.packages(new_packages,verbose=FALSE,quiet=TRUE)
 
@@ -18,3 +18,4 @@ library(ggplot2)
 library(ggsci)
 library(ggpubr)
 library(gridExtra)
+library(tibble)
